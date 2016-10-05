0ab6b3d34ee6736d85e2e6f54edfe3db6ffb7482
Noah Keating <noah@quirky.com>
2016-10-05 18:32:41 -0400

    fixes for compile errors

--- Source/Moya.swift
+++ Source/Moya.swift
@@ -295,8 +295,11 @@ public extension MoyaProvider {
     // These functions are default mappings to MoyaProvider's properties: endpoints, requests, manager, etc.
 
     public final class func DefaultEndpointMapping(target: Target) -> Endpoint<Target> {
-        let url = target.baseURL.URLByAppendingPathComponent(target.path).absoluteString
-        return Endpoint(URL: url, sampleResponseClosure: {.NetworkResponse(200, target.sampleData)}, method: target.method, parameters: target.parameters)
+        if let url = target.baseURL.URLByAppendingPathComponent(target.path) {
+            return Endpoint(URL: url.absoluteString, sampleResponseClosure: {.NetworkResponse(200, target.sampleData)}, method: target.method, parameters: target.parameters)
+        } else {
+            return  Endpoint(URL: target.baseURL, sampleResponseClosure: {.NetworkResponse(200, target.sampleData)}, method: target.method, parameters: target.parameters)
+        }
     }
 
     public final class func DefaultRequestMapping(endpoint: Endpoint<Target>, closure: RequestResultClosure) {
